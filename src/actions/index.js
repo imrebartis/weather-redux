4e1afa86eb4efba3b911c70d3f52ede9719e0538
imrebartis <imbartis@gmail.com>
2017-08-18 06:12:49 +0300

    npm install --save axios; added a fetchWeather action creator (action contains a get request to the API)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = "b069ae3de9b1b1cf9da2edbcfac5a0cf";
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},fi`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
