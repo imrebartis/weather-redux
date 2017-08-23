c26c05e48cf630b483435788bbb500363b60bd73
imrebartis <imbartis@gmail.com>
2017-08-23 06:27:26 +0300

    styles added; Google map shown instead of name of city

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -32,7 +32,7 @@ class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-          placeholder="Get a five-day forecast in your favorite cities"
+          placeholder="Get a five-day forecast in your favorite UK cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange} />
